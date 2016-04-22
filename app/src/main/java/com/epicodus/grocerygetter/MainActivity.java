36725dd763720dd70abbfde40b046259abae2547
chris <cehoughton71@gmail.com>
2016-04-21 20:03:13 -0700

    collect name input and log to logcat

--- app/src/main/java/com/epicodus/grocerygetter/MainActivity.java
+++ app/src/main/java/com/epicodus/grocerygetter/MainActivity.java
@@ -1,24 +1,35 @@
 package com.epicodus.grocerygetter;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mLogInButton;
+    private EditText mNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        mNameEditText = (EditText) findViewById(R.id.nameEditText);
         mLogInButton = (Button) findViewById(R.id.logInButton);
           mLogInButton.setOnClickListener(new View.OnClickListener() {
               @Override
               public void onClick(View view) {
-                  Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_LONG).show();
+                  String name = mNameEditText.getText().toString();
+                  Log.d(TAG, name);
+                  Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                  startActivity(intent);
               }
           });
     }
