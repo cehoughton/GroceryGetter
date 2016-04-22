031131a8a6749f585ab7c11dcb8a32949746b929
chris <cehoughton71@gmail.com>
2016-04-22 09:39:51 -0700

    add listview and arrayadapter

--- app/src/main/java/com/epicodus/grocerygetter/ProfileActivity.java
+++ app/src/main/java/com/epicodus/grocerygetter/ProfileActivity.java
@@ -3,10 +3,14 @@ package com.epicodus.grocerygetter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
     private TextView mProfileTextView;
+    private String[] produceBin = new String[] {"carrots", "squash", "kale", "apples", "pears", "strawberries", "cilantro", "potatoes", "green beans"};
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,9 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         mProfileTextView = (TextView) findViewById(R.id.profileTextView);
+        mListView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, produceBin);
+        mListView.setAdapter(adapter);
 
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
