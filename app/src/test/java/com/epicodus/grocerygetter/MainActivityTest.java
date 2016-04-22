6b78b052317f3a517a2b044645644f1f74171376
chris <cehoughton71@gmail.com>
2016-04-22 14:46:42 -0700

    pass shadow test on main activity

--- app/src/test/java/com/epicodus/grocerygetter/MainActivityTest.java
+++ app/src/test/java/com/epicodus/grocerygetter/MainActivityTest.java
@@ -1,5 +1,6 @@
 package com.epicodus.grocerygetter;
 
+import android.content.Intent;
 import android.os.Build;
 import android.widget.EditText;
 
@@ -9,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
 
 import static junit.framework.Assert.assertTrue;
 
@@ -27,5 +29,14 @@ public class MainActivityTest {
     public void validateTextViewContent() {
         EditText nameEditText = (EditText) activity.findViewById(R.id.nameEditText);
         assertTrue("name".equals(nameEditText.getHint().toString()));
+    }
+
+    @Test
+    public void secondActivityStarted() {
+        activity.findViewById(R.id.logInButton).performClick();
+        Intent expectedIntent = new Intent(activity, ProfileActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
     }
 }
