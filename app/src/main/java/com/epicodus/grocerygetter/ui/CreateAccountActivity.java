82ee6dbc81afb5e8baf5057dbe73c64cdfc3b1d7
chris <cehoughton71@gmail.com>
2016-05-05 16:03:30 -0700

    add automatic authentication after registraition

--- app/src/main/java/com/epicodus/grocerygetter/ui/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/grocerygetter/ui/CreateAccountActivity.java
@@ -1,5 +1,8 @@
 package com.epicodus.grocerygetter.ui;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,10 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.epicodus.grocerygetter.Constants;
 import com.epicodus.grocerygetter.R;
 import com.epicodus.grocerygetter.models.User;
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 
@@ -31,9 +36,14 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     @Bind(R.id.loginTextView)
     TextView mLoginTextView;
     private Firebase mFirebaseRef;
+    private SharedPreferences.Editor mSharedPreferencesEditor;
+    private SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mSharedPreferencesEditor = mSharedPreferences.edit();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
         ButterKnife.bind(this);
@@ -58,6 +68,42 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
             public void onSuccess(Map<String, Object> result){
                 String uid = result.get("uid").toString();
                 createUserInFirebaseHelper(name, email, uid);
+                mFirebaseRef.authWithPassword(email, password, new Firebase.AuthResultHandler() {
+
+                    @Override
+                    public void onAuthenticated(AuthData authData) {
+                        if (authData != null) {
+                            String userUid = authData.getUid();
+
+                            String userInfo = authData.toString();
+                            Log.d(TAG, "Currently logged in: " + userInfo);
+
+                            mSharedPreferencesEditor.putString(Constants.KEY_UID, userUid).apply();
+                            Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onAuthenticationError(FirebaseError firebaseError) {
+                        switch (firebaseError.getCode()) {
+                            case FirebaseError.INVALID_EMAIL:
+                            case FirebaseError.USER_DOES_NOT_EXIST:
+                                mEmailEditText.setError("Please check that you entered your email correctly");
+                                break;
+                            case FirebaseError.INVALID_PASSWORD:
+                                mEmailEditText.setError(firebaseError.getMessage());
+                                break;
+                            case FirebaseError.NETWORK_ERROR:
+                                showErrorToast("There was a problem with the network connection");
+                                break;
+                            default:
+                                showErrorToast(firebaseError.toString());
+                        }
+                    }
+                });
             }
 
             @Override
@@ -66,6 +112,11 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
             }
         });
     }
+
+    private void showErrorToast(String message) {
+        Toast.makeText(CreateAccountActivity.this, message, Toast.LENGTH_LONG).show();
+    }
+
     private void createUserInFirebaseHelper(final String name, final String email, final String uid) {
         final Firebase userLocation = new Firebase(Constants.FIREBASE_URL_USERS).child(uid);
         User newUser = new User(name, email);
